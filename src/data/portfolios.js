9aaa79bf627863183069fae6901b42cfff89e020
gabrielpasquantonio <64622616+gabrielpasquantonio@users.noreply.github.com>
2021-06-05 01:35:02 -0300

    updating projects

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -7,7 +7,9 @@ import img5 from '../img/portImages/antillas.png';
 import img6 from '../img/portImages/form.png';
 import img7 from '../img/portImages/portfolio.png';
 import img8 from '../img/portImages/calculator.png';
-
+import img9 from '../img/portImages/pokemon.png';
+import img10 from '../img/portImages/cripto.png';
+import img11 from '../img/portImages/pern.png';
 
 const portfolios = [
     
@@ -82,6 +84,34 @@ const portfolios = [
         link2: 'https://github.com/gabrielpasquantonio/Vanilla-JavaScript-Calculator',
         title: 'Calculadora JS',
         text: 'Project8'
+    },
+    {
+        id: 9,
+        category: 'React',
+        image: img9,
+        link1: 'https://github.com/gabrielpasquantonio/Pokemon',
+        link2: 'https://pokemon-react-635a4.web.app/',
+        title: 'Pokemon React App',
+        text: 'Project9'
+    },
+    {
+        id: 10,
+        category: 'React',
+        image: img10,
+        link1: 'https://github.com/gabrielpasquantonio/cryptocoin-tracker',
+        link2: 'cointracker-658f0.web.app/',
+        title: 'Criptocoin Api',
+        text: 'Project10'
+    },
+    ,
+    {
+        id: 11,
+        category: 'PostgreSQL',
+        image: img11,
+        link1: 'https://github.com/gabrielpasquantonio/PERN-to-do',
+        link2: 'https://github.com/gabrielpasquantonio/PERN-to-do',
+        title: 'PERN To-do APP',
+        text: 'Project11'
     }
 ]
 
